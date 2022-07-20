aca5ed0d6466af7fc82e06e3df734bf259761fad
sholav18 <sholan.vek@gmail.com>
2022-07-20 09:14:12 -0700

    Pokedex

---
+++ app.js
@@ -0,0 +1,9 @@
+function onload(){
+    console.log(
+        "hello underworld"
+    )
+    const pokemonname= "pokemon name"
+    const pokemonname1= ":lugia"
+    document.getElementById("name").innerHTML = pokemonname + pokemonname1;
+}
+
